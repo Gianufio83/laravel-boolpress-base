e0097d9199ddb944580e84d9d2ba7154e026a84a
Gianufio83 <fiore.gianuario@gmail.com>
2020-03-25 23:01:14 +0100

    creazione seeder

--- database/seeds/ChroniclesTableSeeder.php
+++ database/seeds/ChroniclesTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
+use App\Chronicle;
 class ChroniclesTableSeeder extends Seeder
 {
     /**
@@ -9,8 +10,17 @@ class ChroniclesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newChronicle = new Chronicle();
+            $newChronicle->author = $faker->name();
+            $newChronicle->author_job = $faker->jobTitle();
+            $newChronicle->category = $faker->word();
+            $newChronicle->title = $faker->sentence($nbWords = 15, $variableNbWords = true);
+            $newChronicle->img = $faker->imageUrl($width = 640, $height = 480);
+            $newChronicle->article = $faker->text($maxNbChars = 500);
+            $newChronicle->save();
+        } 
     }
 }
